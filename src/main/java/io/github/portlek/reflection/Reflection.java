c1f3275f653be8ec9280f1c64b4766e01ba99fd1
portlek <hsndmrts_@outlook.com>
2019-07-13 15:59:01 +0300

    last commit

---
+++ src/main/java/io/github/portlek/reflection/Reflection.java
@@ -0,0 +1,225 @@
+package io.github.portlek.reflection;
+
+import org.bukkit.Bukkit;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Reflection {
+
+    private final Logger log;
+
+    public Reflection(Logger log) {
+        this.log = log;
+    }
+
+    public String getCraftBukkitVersion() {
+        return cb().split("\\.")[3];
+    }
+
+    public String getNMSVersion(Object nmsObject) {
+        return nmsObject != null ? nmsObject.getClass().getPackage().getName().split("\\.")[3] : "";
+    }
+
+    public String getNMSVersion() {
+        return nms().split("\\.")[3];
+    }
+
+    public String nms() {
+        Object nmsServer = exec(Bukkit.getServer(), "getServer");
+        return nmsServer != null ? nmsServer.getClass().getPackage().getName() : "net.minecraft.server";
+    }
+
+    public String cb() {
+        return Bukkit.getServer().getClass().getPackage().getName();
+    }
+
+    public String getPackageName(Object nmsObject) {
+        return nmsObject != null ? nmsObject.getClass().getPackage().getName() : "";
+    }
+
+    public Class<?> getBukkitClass(Object craftObject) {
+        Class clazz = craftObject != null ? craftObject.getClass() : null;
+        while (clazz != null && clazz.getCanonicalName().contains(".craftbukkit.")) {
+            clazz = clazz.getSuperclass();
+        }
+        return clazz;
+    }
+
+    public <T> T execStatic(Class<?> clazz, String methodName, Object... args) {
+        try {
+            Class[] argTypes = new Class[args.length];
+            int ix = 0;
+            for (Object arg : args) {
+                argTypes[ix++] = getBukkitClass(arg);
+            }
+            Method method = getMethod(clazz, methodName, argTypes);
+            boolean wasAccessible = method.isAccessible();
+            method.setAccessible(true);
+            try {
+                return (T) method.invoke(null, args);
+            } finally {
+                method.setAccessible(wasAccessible);
+            }
+        } catch (NoSuchMethodException e) {
+            log.fine("Unable to locate method " + methodName + " on " + clazz);
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            log.log(Level.INFO, "Calling " + methodName + " on " + clazz + " threw an exception", e);
+        }
+        return null;
+    }
+
+    public <T> T exec(Object obj, String methodName, Class[] argTypes, Object... args) {
+        if (obj == null) {
+            return null;
+        }
+        Class<?> aClass = obj.getClass();
+        try {
+            Method method = getMethod(aClass, methodName, argTypes);
+            boolean wasAccessible = method.isAccessible();
+            method.setAccessible(true);
+            try {
+                return (T) method.invoke(obj, args);
+            } finally {
+                method.setAccessible(wasAccessible);
+            }
+        } catch (NoSuchMethodException | AbstractMethodError e) {
+            log.fine("Unable to locate method " + methodName + "(" + Arrays.asList(argTypes) + ") on " + aClass);
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            log.log(Level.INFO, "Calling " + methodName + " on " + obj + " threw an exception", e);
+        }
+        return null;
+    }
+
+    public Method getMethod(Class<?> aClass, String methodName, Class... argTypes) throws NoSuchMethodException {
+        try {
+            return aClass.getDeclaredMethod(methodName, argTypes);
+        } catch (NoSuchMethodException e) {
+            return aClass.getMethod(methodName, argTypes);
+        }
+    }
+
+    public Method findMethod(Class<?> aClass, Class returnType, Class... argTypes) throws NoSuchMethodException {
+        List<Method> methods = findMethods(aClass, returnType, argTypes);
+        if (methods.isEmpty()) {
+            throw new NoSuchMethodException("No method matching " + returnType + " ?(" + Arrays.toString(argTypes) + ")");
+        }
+        if (methods.size() > 1) {
+            throw new NoSuchMethodException("More than 1 method matching " + returnType + " ?(" + Arrays.toString(argTypes) + ") : " + methods);
+        }
+        return methods.get(0);
+    }
+
+    public List<Method> findMethods(Class<?> aClass, Class returnType, Class... argTypes) throws NoSuchMethodException {
+        List<Method> methods = new ArrayList<>();
+        for (Method m : aClass.getDeclaredMethods()) {
+            if (m.getReturnType() == returnType && m.getParameterTypes().length == argTypes.length) {
+                try {
+                    Method mLookup = aClass.getMethod(m.getName(), argTypes);
+                    if (mLookup != null) {
+                        methods.add(mLookup);
+                    }
+                } catch (NoSuchMethodException e) {
+                    // ignore...
+                }
+            }
+        }
+        return methods;
+    }
+
+    public <T> T exec(Object obj, String methodName, Object... args) {
+        if (obj == null) {
+            return null;
+        }
+        Class[] argTypes = new Class[args.length];
+        int ix = 0;
+        for (Object arg : args) {
+            argTypes[ix++] = arg != null ? arg.getClass() : null;
+        }
+        return exec(obj, methodName, argTypes, args);
+    }
+
+    public <T> T getField(Object obj, String fieldName) {
+        try {
+            Field field = getFieldInternal(obj, fieldName);
+            boolean wasAccessible = field.isAccessible();
+            field.setAccessible(true);
+            try {
+                return (T) field.get(obj);
+            } finally {
+                field.setAccessible(wasAccessible);
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            log.fine("Unable to find field " + fieldName + " on " + obj);
+        }
+        return null;
+    }
+
+    private Field getFieldInternal(Object obj, String fieldName) throws NoSuchFieldException {
+        return getFieldFromClass(obj.getClass(), fieldName);
+    }
+
+    private Field getFieldFromClass(Class<?> aClass, String fieldName) throws NoSuchFieldException {
+        if (aClass == null) {
+            throw new NoSuchFieldException("Unable to locate field " + fieldName);
+        }
+        try {
+            return aClass.getDeclaredField(fieldName);
+        } catch (NoSuchFieldException e) {
+            // Ignored
+        }
+        try {
+            return aClass.getField(fieldName);
+        } catch (NoSuchFieldException e) {
+            // Ignore
+        }
+        return getFieldFromClass(aClass.getSuperclass(), fieldName);
+    }
+
+    public <T> void setField(Object obj, String fieldName, T field) {
+        try {
+            Field declaredField = getFieldInternal(obj, fieldName);
+            boolean wasAccessible = declaredField.isAccessible();
+            declaredField.setAccessible(true);
+            try {
+                declaredField.set(obj, field);
+            } finally {
+                declaredField.setAccessible(wasAccessible);
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            log.fine("Unable to find field " + fieldName + " on " + obj);
+        }
+    }
+
+    public <T> T newInstance(Class<?> clazz, Object... args) {
+        return newInstance(clazz.getName(), args);
+    }
+
+    public <T> T newInstance(String className, Class<?>[] argTypes, Object... args) {
+        try {
+            Class<?> aClass = Class.forName(className);
+            Constructor<?> constructor = aClass.getDeclaredConstructor(argTypes);
+            return (T) constructor.newInstance(args);
+        } catch (InstantiationException | ClassNotFoundException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            log.warning("Unable to instantiate object of type " + className + ":" + e);
+            return null;
+        }
+    }
+
+    public <T> T newInstance(String className, Object... args) {
+        Class[] argTypes = new Class[args.length];
+        int ix = 0;
+        for (Object arg : args) {
+            argTypes[ix++] = arg != null ? arg.getClass() : null;
+        }
+        return newInstance(className, argTypes, args);
+    }
+
+}
